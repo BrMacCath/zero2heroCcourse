11b38d14493c3aaa71cb21a99e2945d4bc3fe7b2
BrMacCath <briancollery2010@gmail.com>
2025-12-27 19:42:28 +0000

    Prepare for first test

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@ int main(int argc, char *argv[]){
     bool newfile = false;
     int dbfd = -1;
 
+    struct dbheader_t *header = NULL;
+
 	while ((c = getopt(argc, argv, "nf:")) != -1) {
 		switch(c) {
             case 'n':
@@ -44,10 +46,14 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    if(filepath){
+    if(newfile){
         dbfd = create_db_file(filepath);
         if(dbfd == STATUS_ERROR){
             printf("Unable to create database file.\n");
+            return -1;
+        };
+        if (create_db_header(dbfd,&header)== STATUS_ERROR){
+            printf("Failed to create database header");
             return -1;
         }
     }else{
@@ -56,10 +62,16 @@ int main(int argc, char *argv[]){
             printf("Unable to open database file.\n");
             return -1;
         }
+        if(validate_db_header(dbfd, &header) ==STATUS_ERROR){
+            printf("Failed to validate database header\n");
+            return -1;
+        }
     }
 
     printf("Newfile: %d\n", newfile);
     printf("filepath: %s\n", filepath);
+
+    output_file(dbfd,header);
 
 	return 0;
 }
