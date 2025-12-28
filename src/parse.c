eb99ca7f428d6e20fdafed9d8b6b623e838daaab
BrMacCath <briancollery2010@gmail.com>
2025-12-28 14:43:48 +0000

    Testing if this changes things.

--- src/parse.c
+++ src/parse.c
@@ -76,7 +76,7 @@ int validate_db_header(int fd,  struct dbheader_t **header_out){
 
 }
 
-int create_db_header(int fd, struct dbheader_t **header_out){
+int create_db_header(struct dbheader_t **header_out){
     struct dbheader_t *header = calloc(1,sizeof(struct dbheader_t));
 
     if(header ==NULL){
