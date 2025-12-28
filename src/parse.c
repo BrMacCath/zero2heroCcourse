bfb29c969480874beca77f7480e93395f51581eb
BrMacCath <briancollery2010@gmail.com>
2025-12-28 14:39:04 +0000

    Fixed pointer issues

--- src/parse.c
+++ src/parse.c
@@ -37,7 +37,7 @@ int validate_db_header(int fd,  struct dbheader_t **header_out){
     }
 
     struct dbheader_t *header = calloc(1,sizeof(struct dbheader_t));
-    if (header ==-1){
+    if (header == NULL){
         printf("Malloc failed to create a db header.\n");
         return STATUS_ERROR;
     }
@@ -79,7 +79,7 @@ int validate_db_header(int fd,  struct dbheader_t **header_out){
 int create_db_header(int fd, struct dbheader_t **header_out){
     struct dbheader_t *header = calloc(1,sizeof(struct dbheader_t));
 
-    if(header ==-1){
+    if(header ==NULL){
         printf("Malloc failed to create db header.\n");
     }
 
